dc007db730f726cf0867ffbcaf3f53083da6a47b
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2019-04-04 10:20:46 +0200

    funktioner om inloggningsystem och inlägg

--- app.rb
+++ app.rb
@@ -7,3 +7,75 @@ enable :sessions
 get('/') do
     slim(:index)
 end
+
+get('/sign_up') do
+    slim(:sign_up)
+end 
+
+post('/sign_up') do
+    db = SQLite3::Database.new("db/thampis.db")
+    db.results_as_hash = true
+
+    existing_user = db.execute("SELECT username FROM user_login WHERE username=?", params["username"])
+
+    if existing_user.length > 0
+        redirect(:sign_up)
+    end
+
+    password = BCrypt::Password.create(params['password'])
+
+    db.execute("INSERT INTO user_login(username, password) VALUES (?, ?)", [params["username"], password])
+
+    redirect('/')
+end
+
+get('/login') do
+    slim(:login)
+end 
+
+post('/login') do
+    db = SQLite3::Database.new("db/thampis.db")
+    db.results_as_hash = true
+
+    session[:inlagg] = db.execute("SELECT text FROM inlagg WHERE username=?", [params["username"]])
+    inlagg = session[:inlagg]
+
+    existing_user = db.execute("SELECT password FROM user_login WHERE username=?", [params["username"]])
+
+    if existing_user.length == 0
+        redirect(:login)
+    end
+
+    password_hash = existing_user[0]["password"]
+
+    password_match = BCrypt::Password.new(password_hash) == params["password"]
+
+    if password_match
+        session[:username] = params["username"]
+        redirect("/profile/#{params["username"]}")
+    else
+        redirect(:login)
+    end
+end
+
+
+get('/profile/:username') do
+    slim(:profile, locals:{user:params["username"], inlagg: session[:inlagg]})
+    
+end
+
+get('/inlagg') do
+    slim(:inlagg, locals:{user:params["username"], inlagg: session[:inlagg]})
+end 
+
+post('/inlagg') do
+    db = SQLite3::Database.new("db/thampis.db")
+    db.results_as_hash = true
+    
+    db.execute("INSERT INTO inlagg(username, text) VALUES (?,?)", [session[:username], params["text"]])
+
+    session[:inlagg] = db.execute("SELECT text FROM inlagg WHERE username=?", [params["username"]])
+    inlagg = session[:inlagg]
+
+    redirect("/profile/#{session[:username]}")
+end
